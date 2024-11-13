eb2789449008d6a06add61089babb04b29d77296
remles1 <115708921+remles1@users.noreply.github.com>
2024-11-13 22:10:58 +0100

    Add files via upload

---
+++ lab6/cfg.php
@@ -0,0 +1,13 @@
+<?php
+
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $baza ='moja_strona_169328';
+
+    $link = mysqli_connect($dbhost,$dbuser,$dbpass);
+    mysqli_set_charset($link,'utf8mb4');
+    if(!$link) echo '<b>przerwano polaczenie</b>';
+    if(!mysqli_select_db($link,$baza)) echo 'nie wybrano bazy';
+    
+?>
